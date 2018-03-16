a0c190a1712e75152754dcf42e0498a7ce2847bb
NateBowman <natebowman@users.noreply.github.com>
2018-03-16 03:14:37 +0000

    final scene

---
+++ Assets/Scripts/TriggerEvent.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerEvent : MonoBehaviour {
+	public UnityEvent TriggerEnter;
+	public UnityEvent TriggerExit;
+	public UnityEvent TriggerStay;
+
+	private void OnTriggerEnter() {
+		if (TriggerEnter != null) {
+				TriggerEnter.Invoke();
+		}
+	}
+
+	private void OnTriggerExit() {
+		if (TriggerExit != null) {
+			TriggerExit.Invoke();
+		}
+	}
+
+	private void OnTriggerStay() {
+		if (TriggerStay != null) {
+			TriggerStay.Invoke();
+		}
+	}
+}
